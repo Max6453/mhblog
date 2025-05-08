b671b2f0fa87a69beee372acdd5959c930f4cbb6
Maxim Harvancik <maximharvancik@gmail.com>
2025-05-08 20:41:31 +0200

    main

--- app/page.tsx
+++ app/page.tsx
@@ -17,14 +17,14 @@ function classNames(...classes: string[]) {
 export default function Example() {
   return (
   <div>  
-    <Disclosure as="nav" className="bg-white h-30 max-md:h-50 relative max-md:fixed max-md:w-full z-50">
+    <Disclosure as="nav" className="bg-white h-30 max-md:h-40 relative max-md:fixed max-md:w-full z-50">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative h-16 justify-between">
           <h1 className='text-5xl font-Exo-2 pt-5 text-black'>MHBlog</h1>
           <h3 className='text-xl font-edu-vic-wa-nt-beginner pt-2 text-black'>Essays and itriques from many types of media</h3>
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-end left-130 min-md:left-105 max-sm:left-55 pt-10 justify-center rounded-md p-2 text-black hover:bg-white hover:text-blue-500 focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
+            <DisclosureButton className="group relative inline-flex items-end left-130 min-md:left-105 max-sm:left-70 pt-10 justify-center rounded-md p-2 text-black hover:bg-white hover:text-blue-500 focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon aria-hidden="true" className="block size-10 group-data-open:hidden transition-all duration-300" />
@@ -74,8 +74,8 @@ export default function Example() {
       </DisclosurePanel>
     </Disclosure>
 
-    <div className="relative pt-20 max-md:top-50 max-md:pl-3 grid lg:grid-cols-3 md:grid-cols-2 grid-rows-3 gap-10 pl-10 sm:grid-cols-2 -z-10">
-    <div className="bg-white rounded-4xl size-65"></div>
+    <div className="relative pt-20 max-md:top-50 max-md:pl-7 grid lg:grid-cols-3 md:grid-cols-2 grid-rows-3 gap-10 pl-10 sm:grid-cols-2 -z-10">
+    <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65"></div>
     <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65"></div>
     <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65"></div>
     <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65"></div>
@@ -84,7 +84,7 @@ export default function Example() {
     </div>
 
     <div className='bg-white top-70 h-screen relative'>
-      <h1 className='text-5xl font-Exo-2 max-md:text-center text-start md:pl-5 md:pt-10'>About</h1>
+      <h1 className='text-5xl text-black font-Exo-2 max-md:text-center text-start md:pl-5 md:pt-10'>About</h1>
       <p>
         My name is Max. 
       </p>
