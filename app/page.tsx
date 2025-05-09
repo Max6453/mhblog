74c783b8a9f0e903be24dccfb9630ad152c438d0
Maxim Harvancik <maximharvancik@gmail.com>
2025-05-09 20:08:42 +0200

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: 'About', href: '#About', current: true },
   { name: 'Latest', href: '#', current: false },
   { name: 'Categories', href: '#', current: false },
-  { name: 'Contact', href: '#', current: false },
+  { name: 'Contact', href: '#Contact', current: false },
 ]
 
 
@@ -17,17 +18,17 @@ function classNames(...classes: string[]) {
 export default function Main() {
   return (
   <div>  
-    <Disclosure as="nav" className="bg-white h-40 max-md:h-40 relative max-md:fixed max-md:w-full z-50">
+    <Disclosure as="nav" className="bg-white h-40 max-md:h-30 relative max-md:fixed max-md:w-full z-50">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative h-16 justify-between pt-6">
           <h1 className='text-5xl font-Exo-2 text-black'>MHBlog</h1>
           <h3 className='text-xl font-edu-vic-wa-nt-beginner pt-2 text-black'>Essays and itriques from many types of media</h3>
-          <a href='/'>
+          <a href='' className='w-30 h-30 absolute'>
           <img src="/Image.png" className='bottom-24 left-125 items-center relative sm:hidden md:hidden max-md:hidden lg:block'/>
           </a>
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-end max-md:left-165 sm:left-150 max-sm:left-60 pt-10 justify-center rounded-md p-2 text-black hover:bg-white hover:text-blue-500 focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
+            <DisclosureButton className="group relative inline-flex items-end max-md:left-165 sm:left-150 max-sm:left-70 pt-10 justify-center rounded-md p-2 text-black hover:bg-white hover:text-blue-500 focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon aria-hidden="true" className="block size-10 group-data-open:hidden transition-all duration-300" />
@@ -57,7 +58,7 @@ export default function Main() {
           </div>
         </div>
       </div>
-      <DisclosurePanel className="sm:hidden transition-all duration-300 -z-50">
+      <DisclosurePanel className="sm:hidden transition-all duration-300 -z-50 border-t">
         <div className="space-y-1 px-2 pb-5 bg-white pt-25 transition-all duration-300 border-b border-black shadow-2xl">
           {navigation.map((item) => (
             <DisclosureButton
@@ -76,9 +77,13 @@ export default function Main() {
         </div>
       </DisclosurePanel>
     </Disclosure>
-
-    <div className="relative pt-20 max-md:top-50 max-sm:pl-6 max-md:pl-8 grid lg:grid-cols-3 md:grid-cols-2 grid-rows-3 gap-10 pl-10 sm:grid-cols-2 -z-10">
-    <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65"></div>
+      <h1 className='lg:text-7xl max-md:text-5xl text-black relative max-md:top-40 lg:pt-10 pl-10 font-Exo-2'>latest</h1>
+    <div className="relative top-20 max-md:top-50 max-sm:pl-9 max-md:pl-8 grid lg:grid-cols-3 md:grid-cols-2 grid-rows-3 gap-10 pl-10 sm:grid-cols-2 -z-10">
+    <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65 hover:scale-110 transition-all">
+      <a href='#'>
+        <img src='/Image.png' className='hover:scale-120 transition-all'/>
+      </a>
+    </div>
     <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65"></div>
     <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65"></div>
     <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65"></div>
@@ -86,11 +91,14 @@ export default function Main() {
     <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65"></div>
     </div>
 
-    <div className='bg-white max-md:top-70 h-screen relative' id='About'>
-      <h1 className='md:text-3xl lg:text-5xl text-black font-Exo-2 max-md:text-center text-start md:pl-10 md:pt-10'>About</h1>
-      <p className='font-raleway font-medium pt-5 lg:text-2xl lg:w-300 lg:pl-10 text-black'>
+    <div className='bg-white max-md:top-70 h-screen relative min-w-full' id='About'>
+      <h1 className='max-md:text-5xl lg:text-5xl text-black font-Exo-2 max-md:text-center text-start md:pl-10 md:pt-10'>About</h1>
+      <p className='font-raleway font-medium pt-5 pl-2 lg:text-2xl lg:w-300 lg:pl-10 text-black'>
         Hey everyone.<br/>
         My name is Max and I'm 17 years old. I'm from Žilina, Slovakia.
+        This blog was created with a purpose to learn new skills and progress in my learning in web development.
+        Now in lower section you can get to know me and when i finally publish my portfolio, you can see more about me.<br>
+        </br>
         My biggest passion is programming and motorsport. Those two things (and tech) will be the most popular topic on my website.<br/>
         My journey with coding begun in 2022 when i needed a hobby. 
         I wanted to go to the school which specialized in programming but i wasn't accepted. Those from Slovakia known.
@@ -102,12 +110,12 @@ export default function Main() {
       </p>
       <p className='font-raleway font-medium pt-5 lg:text-2xl lg:w-300 lg:pl-10 bg-white text-black'>
         I'm proficient in HTML, CSS.<br/>
-        I'm advanced in Javascript and it's framework - React.<br/>
+        I'm advanced in Javascript and it's framework - React and beginner in next.js.<br/>
         I know basics of SQL. I can work in mySQL, postgreSQL, mongoDB.<br/>
         For design i use figma.<br/>
         And also basics of python i control.<br/>
       </p>
-      <p className='font-raleway font-medium pt-5 lg:text-2xl lg:w-300 lg:pl-10 bg-white text-black'>
+      <p className='font-raleway font-medium pt-5 lg:text-2xl lg:w-300 lg:pl-10 xl:w-full bg-white text-black'>
           Whole section about me was intented to be written as I talking to you.
           But I can assure you when i comes to work, I'm really profesional.
           I'm must say that I'm routine oriented.
@@ -118,6 +126,80 @@ export default function Main() {
         Until then, enjoy my blog.
       </p>
     </div>
+
+       <footer
+       className="footer top-24 border-b text-center border-white grid gap-250 grid-cols-3 grid-rows-1 relative sm:footer-horizontal pt-10 bg-black text-blue-500 border-t font-edu-vic-wa-nt-beginner p-10 text-2xl"
+       id='Contact'>
+          <div className='grid pl-50'>
+            <a href='#'>Motorsport</a>
+            <a href='#'>Tech</a>
+            <a href='#'>Gaming</a>
+            <a href='#'>Articles</a>
+            <a href='#'>Categories</a>
+          </div>
+          <div className='grid'>
+            <a href='mailto:maximharvancik@gmail.com'>My mail</a>
+            <a href='https://www.instagram.com/harvancik_maxim/'>instagram</a>
+            <a href='https://x.com/HarvancikMaxim'>twitter (X)</a>
+            <a href='https://www.linkedin.com/in/maxim-harvancik-b1512a294/'>LinkedIn</a>
+            <a href='#'></a>
+          </div>
+      </footer>
+          <div className="relative isolate overflow-hidden bg-black py-16 sm:py-24 lg:py-32 top-20">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+                <div className="max-w-xl lg:max-w-lg">
+                  <h2 className="text-4xl font-semibold tracking-tight text-white">Subscribe to our newsletter</h2>
+                  <p className="mt-4 text-lg text-gray-300">
+                    Be notify at every event which happened recent days in motorsport, tech, gaming and more.
+                  </p>
+                  <div className="mt-6 flex max-w-md gap-x-4">
+                    <label htmlFor="email-address" className="sr-only">
+                      Email address
+                    </label>
+                    <input
+                      id="email-address"
+                      name="email"
+                      type="email"
+                      required
+                      placeholder="Enter your email"
+                      autoComplete="email"
+                      className="min-w-0 flex-auto rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                    />
+                    <button
+                      type="submit"
+                      className="flex-none rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-cyan-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                    >
+                      Subscribe
+                    </button>
+                  </div>
+                </div>
+                <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+                  <div className="flex flex-col items-start">
+                    <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                      <CalendarDaysIcon aria-hidden="true" className="size-6 text-white" />
+                    </div>
+                    <dt className="mt-4 text-base font-semibold text-white">Weekly articles</dt>
+                    <dd className="mt-2 text-base/7 text-gray-400">
+                      I'm pulbishing every week multiple articles about many topics. Be first to see them out.
+                    </dd>
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                      <HandRaisedIcon aria-hidden="true" className="size-6 text-white" />
+                    </div>
+                    <dt className="mt-4 text-base font-semibold text-white">No spam</dt>
+                    <dd className="mt-2 text-base/7 text-gray-400">
+                      If you subscribed to our newsletter and later changed your mind, don't worry you can unsubscribed any time.
+                    </dd>
+                  </div>
+                </dl>
+              </div>
+            </div>
+        </div>
+    <aside className="bg-black/90 pl-124 top-20 relative max-md:pl-0 max-md:text-lg max-md:text-center">
+        <p className="text-white">Copyright © {new Date().getFullYear()} - All right reserved by MHBlog</p>
+      </aside>
 </div>
   )
 }
