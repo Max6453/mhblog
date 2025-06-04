19ce680b5920a94c68150061f7e23705778f2c75
Maxim harvančík <maximharvancik@gmail.com>
2025-06-04 20:52:32 +0200

    main

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
 import * as React from "react"
  
 import { cn } from "@/lib/utils"
@@ -85,6 +86,26 @@ const components: { title: string; href: string; description: string }[] = [
       "Even i can teach so head to the docs section and get to know about web development",
   },
 ]
+const posts = [
+  {
+    id: 1,
+    title: 'Boost your conversion rate',
+    href: '#',
+    description:
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+    author: {
+      name: 'Michael Foster',
+      role: 'Co-Founder / CTO',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+  // More posts...
+]
 
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
@@ -117,12 +138,9 @@ ListItem.displayName = "ListItem"
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
-
-
 export default function Main() {
-
   return (
-  <div>  
+  <div> 
     <Disclosure as="nav" className="bg-white h-40 max-md:h-35 relative max-md:fixed max-md:w-full z-50">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative h-16 justify-between pt-6">
@@ -307,10 +325,130 @@ export default function Main() {
     </Disclosure>
     {/* END NAVBAR */}
 
-{/* LATEST */}
+    {/* DEMO */}
+     <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+<div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">From the blog</h2>
+          <p className="mt-2 text-lg/8 text-gray-600">Learn how to grow your business with our expert advice.</p>
+        </div>
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {posts.map((post) => (
+            <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+              <div className="flex items-center gap-x-4 text-xs">
+                <time dateTime={post.datetime} className="text-gray-500">
+                  {post.date}
+                </time>
+                <a
+                  href={post.category.href}
+                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                >
+                  {post.category.title}
+                </a>
+              </div>
+              <div className="group relative">
+                <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
+                  <a href={post.href}>
+                    <span className="absolute inset-0" />
+                    {post.title}
+                  </a>
+                </h3>
+                <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{post.description}</p>
+              </div>
+              <div className="relative mt-8 flex items-center gap-x-4">
+                <img alt="" src={post.author.imageUrl} className="size-10 rounded-full bg-gray-50" />
+                <div className="text-sm/6">
+                  <p className="font-semibold text-gray-900">
+                    <a href={post.author.href}>
+                      <span className="absolute inset-0" />
+                      {post.author.name}
+                    </a>
+                  </p>
+                  <p className="text-gray-600">{post.author.role}</p>
+                </div>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </div>
+        <div className="-mt-12 -ml-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+          <h3 className='text-blue-500 text-6xl bold font-Exo-2 pb-3'>latest news</h3>
+          <h4 className='z-50 absolute text-white top-30 pt-3 pl-1 bold font-Exo-2 text-3xl'>Spanish grand prix debriefing</h4>
+          <img
+            alt=""  
+            src="/assets/Spain.jpg"
+            className="w-3xl max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-228"
+          />
+          <a href='/Formula-1/Spanish-Grand-Prix'>
+        <button className='bg-white border border-blue-500 relative bottom-20 max-md:-left-4.5 left-80 text-black font-Exo-2 rounded-full w-45 h-15 hover:bg-blue-500 hover:border-white hover:text-white transition duration-300'>
+          See more</button>
+        </a>
+        </div>
+        <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          <div className="lg:pr-4">
+            <div className="max-w-xl text-base/7 text-gray-700 lg:max-w-lg">
+              <p>
+                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet
+                vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque
+                erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris
+                semper sed amet vitae sed turpis id.
+              </p>
+              <ul role="list" className="mt-8 space-y-8 text-gray-600">
+                <li className="flex gap-x-3">
+                  <CloudArrowUpIcon aria-hidden="true" className="mt-1 size-5 flex-none text-indigo-600" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet
+                    consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
+                    blanditiis ratione.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <LockClosedIcon aria-hidden="true" className="mt-1 size-5 flex-none text-indigo-600" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua
+                    ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <ServerIcon aria-hidden="true" className="mt-1 size-5 flex-none text-indigo-600" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien
+                    vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
+                  </span>
+                </li>
+              </ul>
+              <p className="mt-8">
+                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor
+                fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac
+                adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
+              </p>
+              <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
+              <p className="mt-6">
+                Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh.
+                Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
+                tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam
+                turpis ipsum eu a sed convallis diam.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
+{/* LATEST */}
       <h1 className='lg:text-7xl max-md:text-5xl text-black relative max-md:top-40 lg:pt-10 pl-10 font-Exo-2'>latest</h1>
     <div className="relative top-20 max-md:top-50 max-sm:pl-11.5 max-md:pl-11.5 grid lg:grid-cols-3 md:grid-cols-2 grid-rows-3 gap-10 pl-10 sm:grid-cols-2">
+      <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65 hover:scale-110 transition duration-500">
+        <img src='/assets/Spain.jpg' className='object-cover w-full h-full z-50 rounded-4xl'></img>
+        <span className='relative bottom-66 text-xl pl-8 text-white font-bold font-Exo-2'>Spanish Grand Prix Debriefing</span>
+        <a href='/Formula-1/Spanish-Grand-Prix'>
+        <button className='bg-white border border-blue-500 relative bottom-20 max-md:-left-4.5 left-28 text-black font-Exo-2 rounded-full w-25 h-10 hover:bg-blue-500 hover:border-white hover:text-white transition duration-300'>
+          See more</button>
+        </a>
+      </div>
       <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65 hover:scale-110 transition duration-500">
         <img src='/assets/monaco.jpg' className='object-cover w-full h-full z-50 rounded-4xl'></img>
         <span className='relative bottom-66 text-xl pl-8 text-white font-bold font-Exo-2'>Monaco Grand Prix Debriefing</span>
