a954bf9bfe4aa5670ce493ef0047f1d7fd361a38
Maxim harvančík <maximharvancik@gmail.com>
2025-07-21 12:05:51 +0200

    PWA update

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+const withPWA = require('next-pwa')({
+  dest: 'public', // where the service worker and workbox stuff will go
+  register: true,
+  skipWaiting: true
+});
+
+module.exports = withPWA({
+  // your Next.js config here
+});
+
 export default nextConfig;
