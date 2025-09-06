670485f7f0791615fb8f79470a42f56fe2a0e861
Maxim Harvancik <maximharvancik@gmail.com>
2025-09-06 13:27:45 +0200

    remote push

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jddfPwa6spmPitpY3B4fZwVOX9bhVoqHoys3BUzIc4o=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
