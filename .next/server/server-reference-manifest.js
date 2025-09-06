b62b27708f0345b4616b7c99cc5224d70f1f3978
Maxim harvančík <maximharvancik@gmail.com>
2025-09-06 11:35:07 +0200

    main

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jddfPwa6spmPitpY3B4fZwVOX9bhVoqHoys3BUzIc4o=\"\n}"
---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"__rewrites":{"afterFiles":[],"beforeFiles":[],"fallback":[]},"/_app":["static/chunks/pages/_app.js"],"/_error":["static/chunks/pages/_error.js"],"/api/send-newsletter-email":["static/chunks/pages/api/send-newsletter-email.js"],"sortedPages":["/_app","/_error","/api/send-newsletter-email"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
