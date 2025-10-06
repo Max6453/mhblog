42ed31e8ab7d72b946c90e7871b4fc748226570e
Maxim harvančík <maximharvancik@gmail.com>
2025-10-06 22:09:07 +0200

    new articles - ghost of yotei review, singapore gp report

---
+++ app/Reviews/Gaming/Ghost-of-Yotei/page.tsx
@@ -0,0 +1,136 @@
+  'use client'
+import { ScrollProgress } from "@/components/magicui/scroll-progress";
+import { useState, useEffect } from 'react'
+import { Bars2Icon, CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import * as React from "react"
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { AnimatePresence, motion } from "framer-motion"
+import Header from "@/components/main/header";
+
+export default function AustrianGP() {
+      const [mobileMenuOpen, setMobileMenuOpen] = useState(false) 
+    const [loading, setLoading] = useState(true);
+    return(
+        <div className="bg-background h-full relative">
+        <Header/>
+          <ScrollProgress />
+            <header>
+            <div className="relative inset-0">
+            <h1 className="absolute text-6xl top-82 left-110 z-10 font-josefin-sans max-md:left-7 items-baseline max-md:top-20 md:text-3xl sm:text-xl lg:text-6xl max-md:text-3xl text-white font-bold">
+                Ghost of Yotei review
+            </h1>
+            <h5 className="absolute text-6xl top-100 z-10 left-95 font-sans max-md:left-0 max-md:top-50 md:text-3xl sm:text-xl lg:text-6xl max-md:text-xl text-white font-bold">
+                Evolution or revolution?
+            </h5>
+            <img
+                src="/assets/ghost-of-yotei.png"
+                className="min-w-full transition-transform duration-300 object-cover max-md:size-96 overflow-hidden"
+                alt="Miami Grand Prix"
+            />
+            </div>
+            </header>
+            <div className="relative pb-10 md:left-0">
+                <div className="text-2xl justify-center text-white to-neutral-900 relative text-start">
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-10 max-md:w-90 lg:w-300 font-josefin-sans pt-15">
+                    A successful sequel to hardly beaten predecessor.
+                </p>
+                 <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-0 max-md:w-90 max-lg:w-200 xl:w-300 font-josefin-sans pt-15">
+                    Ghost of yotei story set 300 years after events of ghost of Tsushima.
+                    You play as Atsu who's seek revenge after a 'Yotei six' slaughtered her family.
+                    You will meet many people and decide who you should trust or who you shouldn't.
+                    But be careful, everyone in Ezo wears a mask.
+                </p>
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-0 max-md:w-90 max-lg:w-200 xl:w-300 font-josefin-sans pt-15">
+                    You can see changes from the start.
+                    Many components from ghost of Tsushima were used again like running animation or katana attacks,
+                    but also many things were improved.
+                </p>
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-10 max-md:pl-50 font-josefin-sans pt-15 lg:w-300"> 
+                    In sequel, we now can use multiple weapons instead of stances.
+                    Ghost of Yotei introduced us to katana, dual katana, kusarigama, odachi and yari, which works exactly like stances.
+                    I loved a new mechanic which enables you to throw a weapons from ground to enemies.
+                    From the first sight we can assume that map of ghost of yotei is smaller than from tsushima.
+                    I would say that the map is a little bigger and much more open. You can see campfires with people around it,
+                    or in villages and inn's more people.
+                    Also we changed flute to shamisen and haiku to sum-e painting.
+                </p>
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-10 max-md:pl-50 font-josefin-sans pt-15 lg:w-300">
+                    To be honest, I got mixed feelings.
+                    In some ways, ghost of yotei is better than tsushima, mainly in gameplay, combat, weather dynamic.
+                    However, Tsushima's story is more exciting and exceptional than yotei.
+                    I just liked the idea more of broken samurai who would abandon his way of samurai and his honor to save his home than a bounty hunter who's haunted by the night where her family was killed. 
+                </p>
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-10 max-md:pl-50 font-josefin-sans pt-15 lg:w-300">
+                    Nonetheless, I give ghost of Yotei solid 9/10.
+                    A solid game but strong competition to win a game of the year.
+                </p>
+              </div>
+            </div>
+            <footer
+            className="relative lg:top-0 min-md:top-0 max-md:top-0 max-sm:top-0 sm:top-0 h-full w-full text-center border-white sm:footer-horizontal pt-10 text-black font-edu-vic-wa-nt-beginner p-10 text-2xl"
+            id='Contact'>
+                <div className="relative isolate overflow-hidden py-16 sm:py-24 lg:py-32">
+                    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+                        <div className="max-w-xl lg:max-w-lg">
+                        <h2 className="text-4xl font-semibold tracking-tight text-white">Subscribe to our newsletter</h2>
+                        <p className="mt-4 text-lg text-gray-300">
+                            Be notify at every event which happened recent days in motorsport, tech, gaming and more.
+                        </p>
+                        <div className="mt-6 flex max-w-md gap-x-4">
+                            <label htmlFor="email-address" className="sr-only">
+                            Email address
+                            </label>
+                            <input
+                            id="email-address"
+                            name="email"
+                            type="email"
+                            required
+                            placeholder="Enter your email"
+                            autoComplete="email"
+                            className="min-w-0 flex-auto rounded-md bg-transparent px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-white focus:outline-2 focus:-outline-offset-2 focus:outline-white sm:text-sm/6"
+                            />
+                            <button
+                            type="submit"
+                            className="flex-none rounded-md bg-transparent px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-white hover:text-black focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white border-white border-2 transition duration-300"
+                            id='submitBtn'
+                            >
+                            Subscribe
+                            </button>
+                        </div>
+                        </div>
+                        <dl className="grid relative grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+                        <div className="flex flex-col items-start">
+                            <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                            <CalendarDaysIcon aria-hidden="true" className="size-6 text-white" />
+                            </div>
+                            <dt className="mt-4 text-base font-semibold text-white">Weekly articles</dt>
+                            <dd className="mt-2 text-base/7 text-white">
+                            I'm pulbishing every week multiple articles about many topics. Be first to see them out.
+                            </dd>
+                        </div>
+                        <div className="flex flex-col items-start">
+                            <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                            <HandRaisedIcon aria-hidden="true" className="size-6 text-white" />
+                            </div>
+                            <dt className="mt-4 text-base font-semibold text-white">No spam</dt>
+                            <dd className="mt-2 text-base/7 text-white">
+                            If you subscribed to our newsletter and later changed your mind, don't worry you can unsubscribed any time.
+                            </dd>
+                        </div>
+                        </dl>
+                    </div>
+                    </div>
+                </div>
+                <a href="#top">
+                <svg xmlns="http://www.w3.org/2000/svg" height="50" width="50" viewBox="0 0 384 512" className="absolute right-10 bottom-5">
+                <path fill="#ffffff" d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2 160 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-306.7L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/>
+                </svg></a>
+            </footer>
+            <aside className="relative text-center items-baseline lg:top-0 md:top-0 max-md:top-0 max-sm:top-0 max-md:text-lg max-md:text-center">
+                <p className="text-white">Copyright © {new Date().getFullYear()} - All right reserved by MHBlog</p>
+            </aside>
+        </div>
+    )
+}
