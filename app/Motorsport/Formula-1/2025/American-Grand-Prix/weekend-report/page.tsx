b66fb4a3dabb7e51c53108b0c4f1f3a1a3b310bc
Maxim harvančík <maximharvancik@gmail.com>
2025-10-24 18:17:29 +0200

    race report - COTA

---
+++ app/Motorsport/Formula-1/2025/American-Grand-Prix/weekend-report/page.tsx
@@ -0,0 +1,151 @@
+  'use client'
+import { ScrollProgress } from "@/components/magicui/scroll-progress";
+import { useState, useEffect } from 'react'
+import { Bars2Icon, CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import * as React from "react"
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { AnimatePresence, motion } from "framer-motion"
+import Header from "@/components/main/header";
+
+export default function AustrianGP() {
+      const [mobileMenuOpen, setMobileMenuOpen] = useState(false) 
+    const [loading, setLoading] = useState(true);
+
+    return(
+        <div className="bg-background h-full relative">
+        <Header/>
+          <ScrollProgress />
+            <header>
+            <div className="relative inset-0">
+            <h1 className="absolute text-6xl top-82 left-50 z-10 font-josefin-sans max-md:left-7 items-baseline max-md:top-20 md:text-3xl sm:text-xl lg:text-6xl max-md:text-3xl text-white text-center font-bold">
+                American grand prix - weekend report
+            </h1>
+            <h5 className="absolute text-6xl top-100 z-10 left-70 font-sans max-md:left-0 max-md:top-50 md:text-3xl sm:text-xl lg:text-6xl max-md:text-xl text-white font-bold">
+                Potential three-way championship battle?
+            </h5>
+            <img
+                src="/assets/cota2025.webp"
+                className="min-w-full transition-transform duration-300 object-cover max-md:size-96 overflow-hidden"
+                alt="Miami Grand Prix"
+            />
+            </div>
+            </header>
+            <div className="relative pb-10 md:left-0">
+                <div className="text-2xl justify-center text-white to-neutral-900 relative text-start">
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-10 max-md:w-90 lg:w-300 font-josefin-sans pt-15">
+                    A dominant win for Verstappen and another badly executed race weekend from mclaren.
+                </p>
+                <h3 className="text-4xl m-5 lg:pl-75 pt-5">Friday</h3>
+                 <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-0 max-md:w-90 max-lg:w-200 xl:w-300 font-josefin-sans pt-15">
+                    Friday started really positive from mclaren's.
+                    FP1 was won by LN4 and in sprint qualifying, they looked really good too.
+                    But a Verstappen's exeptional drive overshadowed them.
+                    And Ferrari were in troubles from the first minute of the weekend.
+                    Mercedes, on the other hand, they were in the field of no one.
+                </p>
+                <h3 className="text-4xl m-5 lg:pl-75 pt-5">Saturday</h3>
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-0 max-md:w-90 max-lg:w-200 xl:w-300 font-josefin-sans pt-15">
+                    Sprint was full of action.
+                    First corner in Austin always bring exitment.
+                    In sprint, we saw already 3 retirements in first lap.
+                    After a chains of events, both Mclarens DNF'd in first lap along with Fernando Alonso.
+                    MV1 took his sprint victory with GR63 finishing in P2 and CS55 made it to P3 in sprint.
+                    I personally didn't watch qualifying but after I saw report, I can only say that MV1 smoked whole competition.
+                    Onto the race.
+                </p>
+                <h3 className="text-4xl m-5 lg:pl-75 pt-5">Sunday</h3>
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-10 max-md:pl-50 font-josefin-sans pt-15 lg:w-300"> 
+                    Now, the start was safer this time.
+                    MV1 stayed in P1 but CL16 after a aggresive start on softs overtook LN4 for P2.
+                    His stint on softs was surprising with track temperature rising up to 40 degrees.
+                    Cooling his tyres was essential for potential win or at worst P2.
+                    Meanwhile OP81 overtook GR63 for P5. 
+                    After first pitstops, situation was calm.
+                    Until LN4 came closer to CL16. 
+                    After a long battles at the start of the race, where LN4 had troubles to overtake CL16 ferrari, He finally managed to overtake him on track and finish in P2.
+                    Ferrari's ended in P3 and P4 and OP81 in P5.
+                    But Max Verstappen won 2025 American grand prix and he closed the gap to 40 points.
+                </p>
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-10 max-md:pl-50 font-josefin-sans pt-15 lg:w-300">
+                    This race once again proved why Mclaren didn't deserved to win a constructors.
+                    Yes they design team made a fantasatic job. How they managed to rebuild the car in 2 years is something not many teams can be proud of.
+                    However, their strategy and trackside team needs a rebuild.
+                    So many mistakes from either drivers or whole team is unbearable to watch from champions.
+                    And I don't want to start about their pitstops.
+                    There isn't a race weekend where either Norris or Piastri have a slow stop.
+                    And these mistakes will ultimately cost them a driver championship.
+                </p>
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-10 max-md:pl-50 font-josefin-sans pt-15 lg:w-300">
+                    Pretty exiting race
+                    next stop: Mexico city, Mexico
+                </p>
+              </div>
+            </div>
+            <footer
+            className="relative lg:top-0 min-md:top-0 max-md:top-0 max-sm:top-0 sm:top-0 h-full w-full text-center border-white sm:footer-horizontal pt-10 text-black font-edu-vic-wa-nt-beginner p-10 text-2xl"
+            id='Contact'>
+                <div className="relative isolate overflow-hidden py-16 sm:py-24 lg:py-32">
+                    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+                        <div className="max-w-xl lg:max-w-lg">
+                        <h2 className="text-4xl font-semibold tracking-tight text-white">Subscribe to our newsletter</h2>
+                        <p className="mt-4 text-lg text-gray-300">
+                            Be notify at every event which happened recent days in motorsport, tech, gaming and more.
+                        </p>
+                        <div className="mt-6 flex max-w-md gap-x-4">
+                            <label htmlFor="email-address" className="sr-only">
+                            Email address
+                            </label>
+                            <input
+                            id="email-address"
+                            name="email"
+                            type="email"
+                            required
+                            placeholder="Enter your email"
+                            autoComplete="email"
+                            className="min-w-0 flex-auto rounded-md bg-transparent px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-white focus:outline-2 focus:-outline-offset-2 focus:outline-white sm:text-sm/6"
+                            />
+                            <button
+                            type="submit"
+                            className="flex-none rounded-md bg-transparent px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-white hover:text-black focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white border-white border-2 transition duration-300"
+                            id='submitBtn'
+                            >
+                            Subscribe
+                            </button>
+                        </div>
+                        </div>
+                        <dl className="grid relative grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+                        <div className="flex flex-col items-start">
+                            <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                            <CalendarDaysIcon aria-hidden="true" className="size-6 text-white" />
+                            </div>
+                            <dt className="mt-4 text-base font-semibold text-white">Weekly articles</dt>
+                            <dd className="mt-2 text-base/7 text-white">
+                            I'm pulbishing every week multiple articles about many topics. Be first to see them out.
+                            </dd>
+                        </div>
+                        <div className="flex flex-col items-start">
+                            <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                            <HandRaisedIcon aria-hidden="true" className="size-6 text-white" />
+                            </div>
+                            <dt className="mt-4 text-base font-semibold text-white">No spam</dt>
+                            <dd className="mt-2 text-base/7 text-white">
+                            If you subscribed to our newsletter and later changed your mind, don't worry you can unsubscribed any time.
+                            </dd>
+                        </div>
+                        </dl>
+                    </div>
+                    </div>
+                </div>
+                <a href="#top">
+                <svg xmlns="http://www.w3.org/2000/svg" height="50" width="50" viewBox="0 0 384 512" className="absolute right-10 bottom-5">
+                <path fill="#ffffff" d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2 160 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-306.7L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/>
+                </svg></a>
+            </footer>
+            <aside className="relative text-center items-baseline lg:top-0 md:top-0 max-md:top-0 max-sm:top-0 max-md:text-lg max-md:text-center">
+                <p className="text-white">Copyright © {new Date().getFullYear()} - All right reserved by MHBlog</p>
+            </aside>
+        </div>
+    )
+}
