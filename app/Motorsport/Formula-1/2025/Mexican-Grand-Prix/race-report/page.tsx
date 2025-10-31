0421e45d773ee5e82f69492eaa11f5bfd00aafd8
Maxim harvančík <maximharvancik@gmail.com>
2025-10-31 18:14:07 +0100

    mexican gp article

---
+++ app/Motorsport/Formula-1/2025/Mexican-Grand-Prix/race-report/page.tsx
@@ -0,0 +1,142 @@
+  'use client'
+import { ScrollProgress } from "@/components/magicui/scroll-progress";
+import { useState, useEffect } from 'react'
+import { Bars2Icon, CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import * as React from "react"
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { AnimatePresence, motion } from "framer-motion"
+import Header from "@/components/main/header";
+
+export default function AustrianGP() {
+      const [mobileMenuOpen, setMobileMenuOpen] = useState(false) 
+    const [loading, setLoading] = useState(true);
+
+    return(
+        <div className="bg-background h-full relative">
+        <Header/>
+          <ScrollProgress />
+            <header>
+            <div className="relative inset-0">
+            <h1 className="absolute text-6xl top-82 left-60 z-10 font-josefin-sans max-md:left-7 items-baseline max-md:top-20 md:text-3xl sm:text-xl lg:text-6xl max-md:text-3xl text-white text-center font-bold">
+                Mexican grand prix - race report
+            </h1>
+            <h5 className="absolute text-6xl top-100 z-10 left-40 font-sans max-md:left-0 max-md:top-50 md:text-3xl sm:text-xl lg:text-6xl max-md:text-xl text-white font-bold">
+                New championship leader estabilished
+            </h5>
+            <img
+                src="/assets/Mexico2025.webp"
+                className="min-w-full transition-transform duration-300 object-cover max-md:size-96 overflow-hidden"
+                alt="Miami Grand Prix"
+            />
+            </div>
+            </header>
+            <div className="relative pb-10 md:left-0">
+                <div className="text-2xl justify-center text-white to-neutral-900 relative text-start">
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-10 max-md:w-90 lg:w-300 font-josefin-sans pt-15">
+                    A dominant win by Lando Norris which estabilished him as new championship leader.
+                </p>
+                 <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-0 max-md:w-90 max-lg:w-200 xl:w-300 font-josefin-sans pt-15">
+                    The race started wild.
+                    We had 4 cars side by side down into turn 1.
+                    But, ironically, only 1 managed to stay on track.
+                    A great start by LN4 practically won him his 10th win of his carrer.
+                    But on the other side of garage, we had another battle.
+                    OP81 started from P7 but was demoted to P9 after T1.
+                    However Piastri kept his cool and finished in P5. 
+                    In Mexico, we finally had a full action with 6 drivers.
+                    In T1, we saw hard racing between reign champions MV1 and LH44 and also they collided a little.
+                    In T3, another 4 fight begun with LH44 cutting corners and eventually stewards awarded him 10 second time penalty, MV1 went wide and all of it used Oliver Bearman on Haas who managed to overtake MV1 for P3.
+                    He stayed in P3 until a first round of pitstops. MV1 pace on hards was briliant and Haas is no match for MV1.
+                    but still a unbelieveble result for young briton in Haas.
+                    After what looked a potential race win for ferrari's, It was only a P2 and P8.
+                    Leclerc drove well, even keeping up to LN4 but eventually dropped out.
+                    Hamilton ater 10 second was just slow and couldn't keep up with rest of the pack.
+                    Mercedes, with a little of drama between GR63 and his engineer, only in P6 and P7.
+                    But LN4 won Mexican grand prix by 30 seconds, biggest margin in season so far.
+                    But a little bittersweat win for LN4 because his celebrations must have been soured by booing of mexican fans in dissapproving of team choice in Monza.
+                </p>
+                <h3 className="text-4xl m-5 lg:pl-75 pt-5">FIA...</h3>
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-0 max-md:w-90 max-lg:w-200 xl:w-300 font-josefin-sans pt-15">
+                    FIA.
+                    I just have no words for this clown association.<br/>
+                    1st - couldn't managed to rightfully penalize T1.<br/>
+                    2nd - they let MARSHALLS RUNNING IN T1 WITH CAR DRIVING OVER 130km/h into T1.
+                    Just how dump or short-brained you must be to not fully check if all cars are out of range and even if you make a fatal mistake which fortunately wasn't today, you wont even accept your fault but YOU BLAME A DRIVER FOR NOT MAKING A EFFORT TO DODGE THOSE MARSHALLS!
+                    <br/>
+                    Really FIA is just, like Guenther Steiner would say, a bunch of wankers.
+                </p>
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-10 max-md:pl-50 font-josefin-sans pt-15 lg:w-300"> 
+                    But overall a exiting race which isn't a tradition in Mexico but it was nice to see a change.
+                    <br/>
+                    Next stop: Sao Paulo, Brasil.
+                </p>
+              </div>
+            </div>
+            <footer
+            className="relative lg:top-0 min-md:top-0 max-md:top-0 max-sm:top-0 sm:top-0 h-full w-full text-center border-white sm:footer-horizontal pt-10 text-black font-edu-vic-wa-nt-beginner p-10 text-2xl"
+            id='Contact'>
+                <div className="relative isolate overflow-hidden py-16 sm:py-24 lg:py-32">
+                    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+                        <div className="max-w-xl lg:max-w-lg">
+                        <h2 className="text-4xl font-semibold tracking-tight text-white">Subscribe to our newsletter</h2>
+                        <p className="mt-4 text-lg text-gray-300">
+                            Be notify at every event which happened recent days in motorsport, tech, gaming and more.
+                        </p>
+                        <div className="mt-6 flex max-w-md gap-x-4">
+                            <label htmlFor="email-address" className="sr-only">
+                            Email address
+                            </label>
+                            <input
+                            id="email-address"
+                            name="email"
+                            type="email"
+                            required
+                            placeholder="Enter your email"
+                            autoComplete="email"
+                            className="min-w-0 flex-auto rounded-md bg-transparent px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-white focus:outline-2 focus:-outline-offset-2 focus:outline-white sm:text-sm/6"
+                            />
+                            <button
+                            type="submit"
+                            className="flex-none rounded-md bg-transparent px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-white hover:text-black focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white border-white border-2 transition duration-300"
+                            id='submitBtn'
+                            >
+                            Subscribe
+                            </button>
+                        </div>
+                        </div>
+                        <dl className="grid relative grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+                        <div className="flex flex-col items-start">
+                            <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                            <CalendarDaysIcon aria-hidden="true" className="size-6 text-white" />
+                            </div>
+                            <dt className="mt-4 text-base font-semibold text-white">Weekly articles</dt>
+                            <dd className="mt-2 text-base/7 text-white">
+                            I'm pulbishing every week multiple articles about many topics. Be first to see them out.
+                            </dd>
+                        </div>
+                        <div className="flex flex-col items-start">
+                            <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                            <HandRaisedIcon aria-hidden="true" className="size-6 text-white" />
+                            </div>
+                            <dt className="mt-4 text-base font-semibold text-white">No spam</dt>
+                            <dd className="mt-2 text-base/7 text-white">
+                            If you subscribed to our newsletter and later changed your mind, don't worry you can unsubscribed any time.
+                            </dd>
+                        </div>
+                        </dl>
+                    </div>
+                    </div>
+                </div>
+                <a href="#top">
+                <svg xmlns="http://www.w3.org/2000/svg" height="50" width="50" viewBox="0 0 384 512" className="absolute right-10 bottom-5">
+                <path fill="#ffffff" d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2 160 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-306.7L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/>
+                </svg></a>
+            </footer>
+            <aside className="relative text-center items-baseline lg:top-0 md:top-0 max-md:top-0 max-sm:top-0 max-md:text-lg max-md:text-center">
+                <p className="text-white">Copyright © {new Date().getFullYear()} - All right reserved by MHBlog</p>
+            </aside>
+        </div>
+    )
+}
