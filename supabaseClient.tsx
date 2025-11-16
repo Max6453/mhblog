8134e375dc5d4564e6c281a6baf0bb92c1dc38b2
Maxim harvančík <maximharvancik@gmail.com>
2025-11-16 20:45:06 +0100

    deploy changes

--- supabaseClient.tsx
+++ supabaseClient.tsx
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://dvmcbeouwyfercqjetee.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2bWNiZW91d3lmZXJjcWpldGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5ODE5OTksImV4cCI6MjA2MjU1Nzk5OX0.eI3zNlArlSqgl5zSTbZ73LxMA8zD1oaXbFQ6udioKXE";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
