7d9d4c789a98f31de2847da813d2fc496495c518
Maxim harvančík <maximharvancik@gmail.com>
2025-11-23 17:24:59 +0100

    new mobile app with new name

--- app/page.tsx
+++ app/page.tsx
@@ -231,7 +231,7 @@ export default function Main() {
             </div>
         </div>
         <div className='pt-10 absolute md:left-50 sm:left-15 max-sm:left-14'>
-          <a href='/application-db7221f0-2ea3-4ec9-83b2-038c33a5801e.apk' download>
+          <a href='/mhblog.apk' download>
           <button className='className="px-6 py-3 font-Exo-2 bg-background hover:bg-white text-white hover:text-black border-white border rounded-xl shadow-lg transition'>
             Download our app
           </button>
