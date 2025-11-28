b4c3dd1fed48b36cf33465756f0786d32b6e5bd6
Maxim harvančík <maximharvancik@gmail.com>
2025-11-28 20:19:46 +0100

    removed exesive article better download button placement

--- app/page.tsx
+++ app/page.tsx
@@ -84,14 +84,6 @@ export default function Main() {
           See more</button>
       </div>
       </a> 
-      <a href='/Reviews/Gaming/Ghost-of-Yotei'>
-      <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65 overflow-hidden">
-        <img src='/assets/ghost-of-yotei.png' className=' object-cover w-full h-full z-50 rounded-4xl hover:scale-110 duration-250'/>
-        <h3 className='relative items-center bottom-66 text-xl pl-8 text-white font-bold font-Exo-2'>Ghost of Yotei review</h3>
-        <button className='relative lg:bottom-19 max-md:bottom-27 md:bottom-20 left-1/3 w-25 h-10 rounded-full text-white bg-neutral-950 border border-white hover:bg-white hover:border-black hover:text-black transition duration-300'>
-          See more</button>
-      </div>
-      </a> 
     </div>
    </div>
     {/* END LATEST */}
@@ -238,9 +230,9 @@ export default function Main() {
               </div>
             </div>
         </div>
-        <div className='pt-10 absolute md:left-50 sm:left-15 max-sm:left-14'>
+        <div className='pt-10 max-sm:pt-0 absolute top-100 md:left-65 sm:left-20 max-sm:left-18'>
           <a href='/application-167c16ef-20d8-4555-98a0-3b864bd69ec4.apk' download>
-          <button className='className="px-6 py-3 font-Exo-2 bg-background hover:bg-white text-white hover:text-black border-white border rounded-xl shadow-lg transition'>
+          <button className='className="px-6 py-3 p-3 font-Exo-2 bg-background hover:bg-white text-white hover:text-black border-white border rounded-xl shadow-lg transition'>
             Download our app
           </button>
           </a>
