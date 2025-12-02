17fcef2b0aa8174f1dc42e835c9e38521839b998
Maxim harvančík <maximharvancik@gmail.com>
2025-12-02 16:42:04 +0100

    Qatar grand prix article

---
+++ app/Motorsport/Formula-1/2025/Qatar-Grand-Prix/Race-Report/page.tsx
@@ -0,0 +1,138 @@
+'use client'
+import { ScrollProgress } from "@/components/magicui/scroll-progress";
+import { useState, useEffect } from 'react'
+import { Bars2Icon, CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import * as React from "react"
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { AnimatePresence, motion } from "framer-motion"
+import Header from "@/components/main/header";
+
+export default function AustrianGP() {
+      const [mobileMenuOpen, setMobileMenuOpen] = useState(false) 
+    const [loading, setLoading] = useState(true);
+
+    return(
+        <div className="bg-background h-full relative">
+        <Header/>
+          <ScrollProgress />
+            <header>
+            <div className="relative inset-0">
+            <h1 className="absolute text-6xl top-62 left-52 z-10 font-josefin-sans max-md:left-7 items-baseline max-md:top-20 md:text-3xl sm:text-xl lg:text-6xl max-md:text-3xl text-white text-center font-bold">
+                Qatar Grand Prix - Weekend report
+            </h1>
+            <img
+                src="/assets/Qatar2025.jpg"
+                className="min-w-full transition-transform duration-300 object-cover max-md:size-96 overflow-hidden"
+                alt="Miami Grand Prix"
+            />
+            </div>
+            </header>
+            <div className="relative pb-10 md:left-0">
+                <div className="text-2xl justify-center text-white to-neutral-900 relative text-start">
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-10 max-md:w-90 lg:w-300 font-josefin-sans pt-15">
+                    MV1 Victorious and fair mclaren strikes again.
+                </p>
+                 <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-0 max-md:w-90 max-lg:w-200 xl:w-300 font-josefin-sans pt-15">
+                    Title will be decided in Abu Dhabi as Max Verstappen wins in Qatar.
+                </p>
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-0 max-md:w-90 max-lg:w-200 xl:w-300 font-josefin-sans pt-15">
+                    Whole weekend we knew that this track suits the mclaren. 
+                    But we also knew that Max verstappen will be close.
+                    Whole weekend was dominated by Oscar Piastri who performed like at the beginning of the season.
+                    On the other hand, Lando Norris lost some of his pace from previous races. but also we saw old LN4 from the beginning of the season.
+                    He made multiple mistakes in mainly qualifying sessions.
+                    Ferrari was terible whole weekend. No sign of speed.
+                    Mercedes was competetive the most from the field.
+                </p>
+                <h3 className="text-4xl m-5 lg:pl-75 pt-5">Race</h3>
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-10 max-md:pl-50 font-josefin-sans pt-15 lg:w-300"> 
+                    OP81 managed to stay infront of LN4 and MV1.
+                    MV1 overtook LN4 into T1.
+                    Overall whole left side of the grid had better launch as the left side is better rubbered and have overall better grip.
+                    From this point it was straightforward.
+                    Until 7th lap where PG10 and NH27 collided in T1 which caused a safety car.
+                    17 out of 20 cars pitted for fresher tyres. Only both Mclarens and ocon didn't pitted.
+                    This decision ultimately lost a potential win for OP81 and podium for LN4.
+                    But CS55 capitalised the most of this opportunity which elevated him into P3 where he also finished the race.
+                    Because of the profile of the track, it was imposibble to overtake.
+                    MV1 wins in Qatar Grand Prix tighting the gap to 12 points.
+                    OP81 tightens the gap too to 16 points.
+                    LN4 only needs to finish on the podium in Abu Dhabi to secure the championship.
+                    MV1 with OP81 must win and LN4 needs to be place below from P4 to P6.
+                </p>
+                <p className="text-start text-2xl lg:pl-80 max-sm:pl-2 md:pl-10 max-md:pl-50 font-josefin-sans pt-15 lg:w-300"> 
+                    Lusail circuit was build mainly for motoGP's. It was never a track for F1.
+                    Thus we had nearly to 0 overtakes on track.
+                    Already looking forward to season finale in Abu dhabi.
+                    Last stop: Abu Dhabi, United Arab Emirates.
+                </p>
+              </div>
+             </div>
+            <footer
+            className="relative lg:top-0 min-md:top-0 max-md:top-0 max-sm:top-0 sm:top-0 h-full w-full text-center border-white sm:footer-horizontal pt-10 text-black font-edu-vic-wa-nt-beginner p-10 text-2xl"
+            id='Contact'>
+                <div className="relative isolate overflow-hidden py-16 sm:py-24 lg:py-32">
+                    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+                        <div className="max-w-xl lg:max-w-lg">
+                        <h2 className="text-4xl font-semibold tracking-tight text-white">Subscribe to our newsletter</h2>
+                        <p className="mt-4 text-lg text-gray-300">
+                            Be notify at every event which happened recent days in motorsport, tech, gaming and more.
+                        </p>
+                        <div className="mt-6 flex max-w-md gap-x-4">
+                            <label htmlFor="email-address" className="sr-only">
+                            Email address
+                            </label>
+                            <input
+                            id="email-address"
+                            name="email"
+                            type="email"
+                            required
+                            placeholder="Enter your email"
+                            autoComplete="email"
+                            className="min-w-0 flex-auto rounded-md bg-transparent px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-white focus:outline-2 focus:-outline-offset-2 focus:outline-white sm:text-sm/6"
+                            />
+                            <button
+                            type="submit"
+                            className="flex-none rounded-md bg-transparent px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-white hover:text-black focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white border-white border-2 transition duration-300"
+                            id='submitBtn'
+                            >
+                            Subscribe
+                            </button>
+                        </div>
+                        </div>
+                        <dl className="grid relative grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+                        <div className="flex flex-col items-start">
+                            <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                            <CalendarDaysIcon aria-hidden="true" className="size-6 text-white" />
+                            </div>
+                            <dt className="mt-4 text-base font-semibold text-white">Weekly articles</dt>
+                            <dd className="mt-2 text-base/7 text-white">
+                            I'm pulbishing every week multiple articles about many topics. Be first to see them out.
+                            </dd>
+                        </div>
+                        <div className="flex flex-col items-start">
+                            <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                            <HandRaisedIcon aria-hidden="true" className="size-6 text-white" />
+                            </div>
+                            <dt className="mt-4 text-base font-semibold text-white">No spam</dt>
+                            <dd className="mt-2 text-base/7 text-white">
+                            If you subscribed to our newsletter and later changed your mind, don't worry you can unsubscribed any time.
+                            </dd>
+                        </div>
+                        </dl>
+                    </div>
+                    </div>
+                </div>
+                <a href="#top">
+                <svg xmlns="http://www.w3.org/2000/svg" height="50" width="50" viewBox="0 0 384 512" className="absolute right-10 bottom-5">
+                <path fill="#ffffff" d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2 160 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-306.7L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/>
+                </svg></a>
+            </footer>
+            <aside className="relative text-center items-baseline lg:top-0 md:top-0 max-md:top-0 max-sm:top-0 max-md:text-lg max-md:text-center">
+                <p className="text-white">Copyright © {new Date().getFullYear()} - All right reserved by MHBlog</p>
+            </aside>
+        </div>
+    )
+}
