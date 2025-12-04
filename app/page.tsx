699cf0101eb98074d4582b6ef4214c248050bbc9
Maxim harvančík <maximharvancik@gmail.com>
2025-12-04 20:44:50 +0100

    abu dhabi GP - weekend preview

--- app/page.tsx
+++ app/page.tsx
@@ -36,6 +36,14 @@ export default function Main() {
   <div className="absolute top-0 z-[-2] h-370 w-full"></div>
       <h1 className='lg:text-7xl md:text-5xl max-md:text-5xl text-white relative max-md:top-10 lg:pt-15 pl-10 font-Exo-2'>latest</h1>
     <div className="relative top-20 max-sm:pl-11.5 lg:pl-12 max-md:pl-11.5 md:pl-0 grid lg:grid-cols-2 xl:grid-cols-3 md:grid-cols-2 grid-rows-3 gap-10 pl-10 sm:grid-cols-2">
+      <a href='/Motorsport/Formula-1/2025/Abu-Dhabi-Grand-Prix/Weekend-Preview'>
+      <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65 overflow-hidden">
+        <img src='/assets/abu-dhabi-preview-MCL.jpg' className=' object-cover w-full h-full z-50 rounded-4xl hover:scale-110 duration-250'/>
+        <h3 className='relative items-center bottom-66 text-xl pl-8 linear-90 text-orange-500 font-bold font-Exo-2'>Abu Dhabi Grand Prix - Weekend Preview</h3>
+        <button className='relative lg:bottom-26 max-md:bottom-27 md:bottom-20 left-1/3 w-25 h-10 rounded-full text-white bg-neutral-950 border border-white hover:bg-white hover:border-black hover:text-black transition duration-300'>
+          See more</button>
+      </div>
+      </a> 
       <a href='/Motorsport/Formula-1/2025/Qatar-Grand-Prix/Race-Report'>
       <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65 overflow-hidden">
         <img src='/assets/Qatar2025.jpg' className=' object-cover w-full h-full z-50 rounded-4xl hover:scale-110 duration-250'/>
@@ -73,14 +81,6 @@ export default function Main() {
         <img src='/assets/Brazil2025.avif' className=' object-cover w-full h-full z-50 rounded-4xl hover:scale-110 duration-250'/>
         <h3 className='relative items-center bottom-66 text-xl pl-8 text-white font-bold font-Exo-2'>Brazilian Grand Prix - weekend report</h3>
         <button className='relative lg:bottom-26 max-md:bottom-27 md:bottom-20 left-1/3 w-25 h-10 rounded-full text-white bg-neutral-950 border border-white hover:bg-white hover:border-black hover:text-black transition duration-300'>
-          See more</button>
-      </div>
-      </a> 
-      <a href='/Motorsport/Formula-1/2025/Mexican-Grand-Prix/race-report'>
-      <div className="bg-white rounded-4xl w-90 h-70 max-md:size-65 overflow-hidden">
-        <img src='/assets/Mexico2025.webp' className=' object-cover w-full h-full z-50 rounded-4xl hover:scale-110 duration-250'/>
-        <h3 className='relative items-center bottom-66 text-xl pl-8 text-white font-bold font-Exo-2'>Mexican Grand Prix - race report</h3>
-        <button className='relative lg:bottom-19 max-md:bottom-27 md:bottom-20 left-1/3 w-25 h-10 rounded-full text-white bg-neutral-950 border border-white hover:bg-white hover:border-black hover:text-black transition duration-300'>
           See more</button>
       </div>
       </a> 
